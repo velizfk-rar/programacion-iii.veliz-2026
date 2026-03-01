49439567b0666c3ce55f0862f4a221a927c6c0d9
velizfk <jvelizv6@miumg.edu.gt>
2026-03-01 00:26:32 -0600

    Update lista_productos.cpp

--- Tarea_Semana_IV/lista_productos.cpp
+++ Tarea_Semana_IV/lista_productos.cpp
@@ -16,72 +16,104 @@ struct Nodo{
 
 int main(){
 
+
+    cout << "José Daniel Véliz Velásquez\n";
+    cout << "9941-24-12576\n";
+
+    cout << "Ingrese datos de los 4 productos: \n\n";
+
     //Creacion de primer nodo
 
     Nodo*primero = new Nodo;
 
-    primero -> data.codigo = 101;
-    primero -> data.nombre = "Laptop";
-    primero -> data.precio = 5500.50;
+    cout <<"Producto 1: \n";
+    cout << "Código: "; cin >> primero -> data.codigo;
+    cout << "Nombre: "; cin.ignore();
+    getline(cin, primero->data.nombre);
+    cout << "Precio: "; cin >> primero -> data.precio;
 
     primero ->siguiente = nullptr;
+    cout << "-----------------\n";
 
     //Creacion de segundo nodo
 
     Nodo*segundo = new Nodo;
 
-    segundo -> data.codigo = 102;
-    segundo -> data.nombre = "Mouse";
-    segundo -> data.precio = 150.75;
+    cout <<"Producto 2: \n";
+    cout << "Código: "; cin >> segundo -> data.codigo;
+    cout << "Nombre: "; cin.ignore();
+    getline(cin, segundo->data.nombre);
+    cout << "Precio: "; cin >> segundo -> data.precio;
 
     //Conexión de primer nodo con segundo nodo
 
     segundo ->siguiente = nullptr;
     
     primero ->siguiente = segundo;
+    cout << "-----------------\n";
 
     //Creacion de tercer nodo
 
     Nodo*tercero = new Nodo;
 
-    tercero -> data.codigo = 103;
-    tercero -> data.nombre = "Teclado";
-    tercero -> data.precio = 330;
+    cout <<"Producto 3: \n";
+    cout << "Código: "; cin >> tercero -> data.codigo;
+    cout << "Nombre: "; cin.ignore();
+    getline(cin, tercero->data.nombre);
+    cout << "Precio: "; cin >> tercero -> data.precio;
 
     //Conexión de segundo nodo con tercer nodo
 
     tercero ->siguiente = nullptr;
     
     segundo ->siguiente = tercero;
+    cout << "-----------------\n";
     
 
     //Creacion de cuarto nodo
 
     Nodo*cuarto = new Nodo;
 
-    cuarto -> data.codigo = 104;
-    cuarto -> data.nombre = "Monitor";
-    cuarto -> data.precio = 799;
+    cout <<"Producto 4: \n";
+    cout << "Código: "; cin >> cuarto -> data.codigo;
+    cout << "Nombre: "; cin.ignore();
+    getline(cin, cuarto->data.nombre);
+    cout << "Precio: "; cin >> cuarto -> data.precio;
 
     //Conexión de segundo nodo con tercer nodo
 
     cuarto ->siguiente = nullptr;
     
-    tercero ->siguiente = cuarto;    
+    tercero ->siguiente = cuarto;  
+    cout << "-----------------\n";
+    cout << "-----------------\n"; 
+    cout << "-----------------\n"; 
+    cout << "-----------------\n"; 
+
+
+    double total = 0;
 
     Nodo*actual = primero;
     while(actual != nullptr){
 
         cout << "Codigo: " << actual->data.codigo<< endl;
-        cout << "Codigo: " << actual->data.nombre<< endl;
-        cout << "Codigo: " << actual->data.precio<< endl;
-        cout << "Codigo: " << "-----------------"<< endl;
+        cout << "Nombre: " << actual->data.nombre<< endl;
+        cout << "Precio: " << actual->data.precio<< endl;
+        cout << "-----------------\n"<< endl;
 
+
+        total += actual -> data.precio;
         actual = actual->siguiente; 
 
+        
     }
     
+        cout << "\nTotal acumulado: Q" << total;
+
+
     //Limpieza de memoria
+
+    actual = primero;
 
     while(actual != nullptr){
 
@@ -90,7 +122,7 @@ int main(){
         delete temp;
     }
 
-    primero = nullptr;
+    return 0;
 
 
     //José Daniel Véliz Velásquez
