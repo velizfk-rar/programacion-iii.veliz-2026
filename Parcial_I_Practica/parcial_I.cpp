8e60bd32615b097c82ded780728a73aec86f5ae0
velizfk <jvelizv6@miumg.edu.gt>
2026-03-01 13:51:53 -0600

    Create parcial_I.cpp

---
+++ Parcial_I_Practica/parcial_I.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+using namespace std;
+
+struct Producto{
+    int codigo;
+    string nombre;
+    double precio;
+};
+
+struct Nodo{
+
+    Producto data;
+    Nodo * siguiente;
+};
+
+
+int main(){
+
+
+    cout << "José Daniel Véliz Velásquez\n";
+    cout << "9941-24-12576\n";
+
+    cout << "Ingrese datos de los 4 productos: \n\n";
+
+    //Creacion de primer nodo
+
+    Nodo*primero = new Nodo;
+
+    cout <<"Producto 1: \n";
+    cout << "Codigo: "; cin >> primero -> data.codigo;
+    cout << "Nombre: "; cin.ignore();
+    getline(cin, primero->data.nombre);
+    cout << "Precio: "; cin >> primero -> data.precio;
+
+    primero ->siguiente = nullptr;
+    cout << "-----------------\n";
+
+    //Creacion de segundo nodo
+
+    Nodo*segundo = new Nodo;
+
+    cout <<"Producto 2: \n";
+    cout << "Codigo: "; cin >> segundo -> data.codigo;
+    cout << "Nombre: "; cin.ignore();
+    getline(cin, segundo->data.nombre);
+    cout << "Precio: "; cin >> segundo -> data.precio;
+
+    //Conexión de primer nodo con segundo nodo
+
+    segundo ->siguiente = nullptr;
+    
+    primero ->siguiente = segundo;
+    cout << "-----------------\n";
+
+    //Creacion de tercer nodo
+
+    Nodo*tercero = new Nodo;
+
+    cout <<"Producto 3: \n";
+    cout << "Codigo: "; cin >> tercero -> data.codigo;
+    cout << "Nombre: "; cin.ignore();
+    getline(cin, tercero->data.nombre);
+    cout << "Precio: "; cin >> tercero -> data.precio;
+
+    //Conexión de segundo nodo con tercer nodo
+
+    tercero ->siguiente = nullptr;
+    
+    segundo ->siguiente = tercero;
+    cout << "-----------------\n";
+    cout << "Visualizacion de valores ingresados: \n";
+    cout << "-----------------\n";
+    
+    Nodo*actual = primero;
+    while(actual != nullptr){
+
+        cout << "Codigo: " << actual->data.codigo<< endl;
+        cout << "Nombre: " << actual->data.nombre<< endl;
+        cout << "Precio: " << actual->data.precio<< endl;
+        cout << "-----------------\n"<< endl;
+
+
+        actual = actual->siguiente; 
+
+        
+    }
+    
+    //Limpieza de memoria
+
+    actual = primero;
+
+    while(actual != nullptr){
+
+        Nodo* temp = actual;
+        actual = actual->siguiente;
+        delete temp;
+    }
+
+    return 0;
+
+
+    //José Daniel Véliz Velásquez
+    //9941 - 24 - 12576
+    //Tarea #3 - Estructuras, Nodos y Listas simples
+
+
+}
